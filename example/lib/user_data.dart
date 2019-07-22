67d4bc62bb29267fe69a655fed8e7843ffbc262d
qixinbing <qixinbing@rongcloud.cn>
2019-07-22 12:40:08 +0800

    问题:删除敏感数据 原因: 解决方案: Reviewer: 工单号: Log: X

--- example/lib/user_data.dart
+++ example/lib/user_data.dart
@@ -1,4 +1,4 @@
 
-const String AppKey = "uwd1c0sxuqp91";
-const String CurrentUserId = "flutter_android0";
-const String IMToken = "dQWjrThJfKuxq7sO2kL2wJw8RbidCT4Kca/EOv2J27k5Kg3OPb2V4kdEksa3tFZ+gj5mY9RZu20VKzHpH6AW984ln7DVUqldg3yliszv+tg=";
+const String AppKey = "";
+const String CurrentUserId = "";
+const String IMToken = "";
