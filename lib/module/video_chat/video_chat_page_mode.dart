a81664a449776a01f63be8b2135589dd7cc6711c
panmingda <panmingda@rongcloud.cn>
2020-10-12 16:20:24 +0800

    delete useless files

--- lib/module/video_chat/video_chat_page_mode.dart
+++
@@ -1,6 +0,0 @@
-import 'package:FlutterRTC/frame/template/mvp/model.dart';
-import 'package:FlutterRTC/module/video_chat/video_chat_page_contract.dart';
-
-class VideoChatPageModel extends AbstractModel implements Model {
-
-}
